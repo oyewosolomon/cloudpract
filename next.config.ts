8bd0c4a1c46e1d280caf1b376d450debae18ecb1
oyewosolomon <oyewosolomon@gmail.com>
2025-03-26 15:32:59 +0100

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export mode
+  images: {
+    unoptimized: true, // Required if using next/image
+  },
 };
 
 export default nextConfig;
